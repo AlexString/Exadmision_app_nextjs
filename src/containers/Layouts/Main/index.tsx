274f5a9db7c274fae53a08908167b743e3e32fef
AlexString <holacebelogt@gmail.com>
2022-05-22 16:05:10 -0500

    add: acera de nosotros to Layouts component

--- src/containers/Layouts/Main/index.tsx
+++ src/containers/Layouts/Main/index.tsx
@@ -1,4 +1,5 @@
 import React, { Children, FC } from 'react'
+import Link from 'next/link';
 
 import styled from 'styled-components';
 
@@ -6,7 +7,16 @@ interface IMainLayoutProps {}
 const MainLayout:FC<IMainLayoutProps> = ({children}) => {
 	return (
 		<Layout>
-			<div className='header'></div>
+			<div className='header'>
+				<div className='header-content'>
+					<span className='title'>Exadmision App</span>
+					<div className='right-content'>
+						<Link href="">
+							<a>Acerca de nosotros</a>
+						</Link>
+					</div>
+				</div>
+			</div>
 			<div className='content'>{children}</div>
 		</Layout>
 	)
@@ -18,6 +28,32 @@ const Layout = styled.div`
 		width: 100%;
 		background-color: #006994;
 		box-shadow: 2px 0px 3px;
+		display: flex;
+		align-items: center;
+	}
+	.header-content {
+		display: flex;
+		align-items: center;
+		justify-content: space-between;
+		padding-left: 10px;
+		padding-right: 10px;
+		width: 100%;
+		color: white;
+		text-underline: 1px;
+	}
+	.content {
+	}
+	.right-content{
+		display: flex;
+		justify-content: end;
+		font-size: 20px;
+		a:visited {
+			color: white;
+		}
+	}
+	.title {
+		font-size: 24px;
+		color: white;
 	}
 	.content {
 		position: relative;
